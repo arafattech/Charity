7569c37444e83b372e8a5cace8ba7bd9ea186fb6
arafattech <Hybritech@2024>
2025-11-26 23:25:46 +0600

    genareat

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { GetInvolvedComponent } from './pages/get-involved/get-involved.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'get-involved', component: GetInvolvedComponent },
   { path: '**', redirectTo: '' }
 ];
+
 
 
